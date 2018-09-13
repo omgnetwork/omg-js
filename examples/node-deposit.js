ead2aacf30c286d230d31377c2779e61a8ea88ba
Pong Cheecharern <pongcheecharern@MacBook-Pro-5.local>
2018-09-13 21:21:45 +0700

    working deposit -> transact from end to end

--- examples/node-deposit.js
+++ examples/node-deposit.js
@@ -4,15 +4,17 @@
 
 let gethRpc = "http://localhost:8545"
 
-const alice = "0x25d274f6ec8a14bba45de2b4581203e65f8a9060"
+const alice = "0x2eac736d6f0d71d3e51345417a5b205bfa4748a8"
 
 const OMG = require('../omg')
-const Omg = new OMG("watcher_url", "childChainLocal", "http://localhost:8545", "0x96f770ffab0b6094c52135a6129f4d2bb5089f83")
+const Omg = new OMG("watcher_url", "childChainLocal", "http://localhost:8545", "0x733bf3b72381f4c8ae9b2952e6da36deb18297eb")
 
 async function depositAndGetBlock(){
-  const depositEth = require('../deposit/depositEth')
-  let deposited = await depositEth("1", "0x25d274f6ec8a14bba45de2b4581203e65f8a9060", "0x96f770ffab0b6094c52135a6129f4d2bb5089f83","http://localhost:8545")
-  let blockNum = await Omg.getDepositBlock(deposited)
+  //const depositEth = require('../deposit/depositEth')
+  //let deposited = await depositEth("1", alice, "0x733bf3b72381f4c8ae9b2952e6da36deb18297eb","http://localhost:8545")
+  let depositEth = await Omg.depositEth("10", alice)
+  let blockNum = await Omg.getDepositBlock(depositEth)
 }
 
 depositAndGetBlock()
+
