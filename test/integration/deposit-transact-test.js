1d0b090e5e8b322bb5b524036492708a9d20333e
Pong Cheecharern <pongcheecharern@MacBook-Pro-5.local>
2018-09-14 19:41:50 +0700

    improved comments on integration

--- test/integration/deposit-transact-test.js
+++ test/integration/deposit-transact-test.js
@@ -19,14 +19,11 @@ const alice = "0x32c31f4c277e050a97e1bee6a619f70627de8570"
 const bob = "0x1522579be266dcbdae704b4129cf0d30dc99b51e"
 const alicePriv = "0x8f265d5e82cb327c687222d0a9de4178ad9b40922f9dda08932b5319d5aa4a9d"
 
-async function wait() {
-  await sleep(20000)
-}
-
-//mocha --timeout 20000 deposit-transact-test
 
 async function depositAndTransact(){
+  console.log("===== alice deposit 2 ETH")
   let depositEth = await Omg.depositEth("2", alice)
+  console.log("===== alice retrieves the blocknumber of her deposit")
   let depositedBlockNum = await Omg.getDepositBlock(depositEth)
   let input_1 = { blknum1: depositedBlockNum, txindex1: 0, oindex1: 0 }
   let input_2 = { blknum2: 0, txindex2: 0, oindex2: 0 }
@@ -35,14 +32,18 @@ async function depositAndTransact(){
       { newowner1: alice, amount1: 1 },
       { newowner2: bob, amount2: 1 } 
   ]
+  console.log("===== alice wait for deposit to go through")
   await wait()
+  console.log("===== alice transfer 1 ETH to bob")
   return await Omg.sendTransaction([input_1, input_2], curr , output, alicePriv)
 }
 
+async function wait() {
+  await sleep(20000)
+}
 
-
-describe('deposit and transact', async () => {
-  it('should deposit, transact, and then returns object with result', async () => {
+describe('alice make an ETH deposit and then transact to bob', async () => {
+  it('should deposit, transact, and then returns an object with correct result', async () => {
       //transactionResult 
     let txResult = {
         jsonrpc:"2.0",
