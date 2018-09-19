5a02f904df5b711c4df38f7a423c424d65366134
Pong Cheecharern <pongcheecharern@MacBook-Pro-5.local>
2018-09-19 18:39:31 +0700

    revert some changes in node-sendTx example

--- examples/node-sendTx.js
+++ examples/node-sendTx.js
@@ -1,20 +1,21 @@
 //example node app to run OMG.JS Library 
 //IMPORTANT: Do not store Privatekey as strings in production apps
 
+
+//let childChainPort = "http://35.200.30.83:9656"
 let childChainLocal = "http://localhost:9656"
 
 const OMG = require('../omg')
 const Omg = new OMG("watcher_url", childChainLocal, "web3_provider", "plasma_addr")
 
 
-let inputz_1 = { blknum1: 4769001, txindex1: 0, oindex1: 0 }
+let inputz_1 = { blknum1: 1404001, txindex1: 0, oindex1: 0 }
 let inputz_2 = { blknum2: 0, txindex2: 0, oindex2: 0 }
-let currz = [ 33, 128, 23, 181, 247, 195, 241, 165, 141, 222, 158, 11, 148, 93, 30, 46, 13, 249, 216, 186 ]
+let currz = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
 let ownerz = [ 
-    { newowner1: '0x95d6c5cf598adbd58ce2e169cde751fc51109f8b', amount1: 1 },
-    { newowner2: '0x1522579be266dcbdae704b4129cf0d30dc99b51e', amount2: 1 } 
+    { newowner1: '0x2eac736d6f0d71d3e51345417a5b205bfa4748a8', amount1: 3 },
+    { newowner2: '0x542740fabe3222a5a4e3c8521f63ab74ac6c77d9', amount2: 2 } 
 ]
-let alicePriv = "0x9756b46cfd1997131a4987b2f534cd93727f54c1351d8770a3e8519342f26b03"
+let alicePriv = "0xe6dfd35b7c5b4f2e69b57756c926a89b185c5e7e0551a604c890e6a840192ae4"
 
 Omg.sendTransaction([inputz_1, inputz_2], currz , ownerz, alicePriv)
-
