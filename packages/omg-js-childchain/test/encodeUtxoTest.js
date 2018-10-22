9920b1a5a7d3b4fd891923f2aa510d60785cd58f
Kevin Sullivan <kevsul@gmail.com>
2018-10-22 22:02:47 +0200

    Use BN.js to calculate encoded Utxo Pos

---
+++ packages/omg-js-childchain/test/encodeUtxoTest.js
@@ -0,0 +1,44 @@
+const chai = require('chai')
+const assert = chai.assert
+const transcation = require('../src/transaction/transaction')
+
+describe.only('Encode UTXO tests', () => {
+  it('should return the encoded utxo position', () => {
+    const utxo = {
+      blknum: 96035000,
+      currency: '',
+      oindex: 0,
+      txbytes: '',
+      txindex: 0
+    }
+
+    const utxoPos = transcation.encodeUtxoPos(utxo)
+    assert.equal(utxoPos.toString(), '96035000000000000')
+  })
+
+  it('should return the encoded utxo position', () => {
+    const utxo = {
+      blknum: 96035000,
+      currency: '',
+      oindex: 1,
+      txbytes: '',
+      txindex: 7
+    }
+
+    const utxoPos = transcation.encodeUtxoPos(utxo)
+    assert.equal(utxoPos.toString(), '96035000000070001')
+  })
+
+  it('should return the encoded utxo position', () => {
+    const utxo = {
+      blknum: 777777777,
+      currency: '',
+      oindex: 999,
+      txbytes: '',
+      txindex: 8888
+    }
+
+    const utxoPos = transcation.encodeUtxoPos(utxo)
+    assert.equal(utxoPos.toString(), '777777777088880999')
+  })
+})
