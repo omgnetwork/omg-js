096451a5c508bd89fb414e52fe4dc86472687814
Pong Cheecharern <k.cheecharern@gmail.com>
2018-11-06 12:56:41 +0100

    merge

--- packages/omg-js-rootchain/src/index.js
+++ packages/omg-js-rootchain/src/index.js
@@ -57,10 +57,10 @@ class RootChain {
   }
 
   /**
-   * deposit Token to rootchain
+   * deposit Token to rootchain (caller must be token owner)
    *
    * @method depositToken
-   * @param {number} amount amount of ETH to deposit
+   * @param {number} amount amount of ETH to deposit 
    * @param {string} fromAddress address to make the deposit from
    * @param {string} tokenAddress address of the ERC20 Token
    * @param {string} privateKey private key to sign the transaction with. If not set, assume sending from an unlocked geth account
@@ -71,7 +71,7 @@ class RootChain {
     const txDetails = {
       from: fromAddress,
       to: this.plasmaContractAddress,
-      data: this.plasmaContract.methods.depositFrom(fromAddress, tokenAddress, amount).encodeABI(),
+      data: this.plasmaContract.methods.depositFrom(tokenAddress, amount).encodeABI(),
       gas: 2000000
     }
 
