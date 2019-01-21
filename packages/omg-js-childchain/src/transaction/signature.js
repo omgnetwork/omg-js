513c014abda5791adb25aeb636756af30a5c5ad9
Kevin Sullivan <kevsul@gmail.com>
2019-01-21 15:48:42 +0100

    Don't need to pad a tx with 'NULL' sigs anymore

--- packages/omg-js-childchain/src/transaction/signature.js
+++ packages/omg-js-childchain/src/transaction/signature.js
@@ -19,8 +19,6 @@ const keccak256 = require('js-sha3').keccak256
 const ethUtil = require('ethereumjs-util')
 const sigUtil = require('eth-sig-util')
 
-const NULL_SIG = '0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-
 function ecSign (tosign, privateKey) {
   const hashed = keccak256(tosign)
   const signed = ethUtil.ecsign(
@@ -31,21 +29,7 @@ function ecSign (tosign, privateKey) {
 }
 
 function sign (tx, privateKeys) {
-  // Currently transaction always have 2 inputs.
-  // If no private key is given for an input, then we sign with a 'null' signature
-  const REQUIRED_SIGNATURES = 2
-  const signatures = []
-
-  for (let i = 0; i < REQUIRED_SIGNATURES; i++) {
-    if (privateKeys.length > i) {
-      signatures.push(ecSign(tx, privateKeys[i]))
-    } else {
-      // Use null sig for the second signature
-      signatures.push(NULL_SIG)
-    }
-  }
-
-  return signatures
+  return privateKeys.map(key => ecSign(tx, key))
 }
 
 module.exports = sign
