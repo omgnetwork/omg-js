2cc362d473d10f3bb46d1478543b46d0d8ebf45b
Kevin Sullivan <kevsul@gmail.com>
2019-01-21 10:28:07 +0100

    Update omg-js-rootchain/index.js to new RootChain.sol interface

--- packages/omg-js-rootchain/src/index.js
+++ packages/omg-js-rootchain/src/index.js
@@ -35,106 +35,117 @@ class RootChain {
   }
 
   /**
-   * deposit ETH to rootchain
+   * Deposit ETH to rootchain
    *
    * @method depositEth
+   * @param {string} depositTx RLP encoded childchain deposit transaction
    * @param {number} amount amount of ETH to deposit
-   * @param {string} fromAddress address to make the deposit from
-   * @param {string} privateKey private key to sign the transaction with. If not set, assume sending from an unlocked geth account
-   * @return {string} transaction Hash of the deposit
+   * @param {object} txOptions transaction options, such as `from`, gas` and `privateKey`
+   * @return {string} transaction hash of the call
    */
 
-  async depositEth (amount, fromAddress, privateKey) {
+  async depositEth (depositTx, amount, txOptions) {
     const txDetails = {
-      from: fromAddress,
+      from: txOptions.from,
       to: this.plasmaContractAddress,
       value: amount,
-      data: this.plasmaContract.methods.deposit().encodeABI(),
-      gas: 2000000
+      data: this.plasmaContract.methods.deposit(depositTx).encodeABI(),
+      gas: txOptions.gas || 2000000
     }
 
-    return sendTx(this.eth, txDetails, privateKey)
+    return sendTx(this.eth, txDetails, txOptions.privateKey)
   }
 
   /**
-   * deposit Token to rootchain (caller must be token owner)
+   * Deposit ERC20 Token to rootchain (caller must be token owner)
    *
    * @method depositToken
-   * @param {number} amount amount of ETH to deposit
-   * @param {string} fromAddress address to make the deposit from
-   * @param {string} tokenAddress address of the ERC20 Token
-   * @param {string} privateKey private key to sign the transaction with. If not set, assume sending from an unlocked geth account
-   * @return {string} transaction Hash of the deposit
+   * @param {string} depositTx RLP encoded childchain deposit transaction
+   * @param {object} txOptions transaction options, such as `from`, gas` and `privateKey`
+   * @return {string} transaction hash of the call
    */
 
-  async depositToken (amount, fromAddress, tokenAddress, privateKey) {
+  async depositToken (depositTx, txOptions) {
     const txDetails = {
-      from: fromAddress,
+      from: txOptions.from,
       to: this.plasmaContractAddress,
-      data: this.plasmaContract.methods.depositFrom(tokenAddress, amount).encodeABI(),
-      gas: 2000000
+      data: this.plasmaContract.methods.depositFrom(depositTx).encodeABI(),
+      gas: txOptions.gas || 2000000
     }
 
-    return sendTx(this.eth, txDetails, privateKey)
+    return sendTx(this.eth, txDetails, txOptions.privateKey)
   }
 
-  async startExit (fromAddress, utxoPos, txBytes, proof, sigs, privateKey) {
+  /**
+   * Starts a standard withdrawal of a given output. Uses output-age priority.
+   *
+   * @method startStandardExit
+   * @param {number} outputId Identifier of the exiting output.
+   * @param {string} outputTx RLP encoded transaction that created the exiting output.
+   * @param {string} inclusionProof A Merkle proof showing that the transaction was included.
+   * @param {object} txOptions transaction options, such as `from`, gas` and `privateKey`
+   * @return {string} transaction hash of the call
+   */
+  async startStandardExit (outputId, outputTx, inclusionProof, txOptions) {
     const txDetails = {
-      from: fromAddress,
+      from: txOptions.from,
       to: this.plasmaContractAddress,
       data: this.plasmaContract.methods.startExit(
-        utxoPos,
-        Web3Utils.hexToBytes(`0x${txBytes}`),
-        Web3Utils.hexToBytes(`0x${proof}`),
-        Web3Utils.hexToBytes(`0x${sigs}`)
+        outputId,
+        Web3Utils.hexToBytes(`0x${outputTx}`),
+        Web3Utils.hexToBytes(`0x${inclusionProof}`)
       ).encodeABI(),
-      gas: 2000000
+      gas: txOptions.gas || 2000000
     }
 
-    return sendTx(this.eth, txDetails, privateKey)
+    return sendTx(this.eth, txDetails, txOptions.privateKey)
   }
 
-  async startDepositExit (fromAddress, depositPos, token, amount, privateKey) {
+  /**
+   * Blocks a standard exit by showing the exiting output was spent.
+   *
+   * @method challengeStandardExit
+   * @param {number} outputId Identifier of the exiting output.
+   * @param {string} challengeTx RLP encoded transaction that spends the exiting output.
+   * @param {number} inputIndex Which input to the challenging tx corresponds to the exiting output.
+   * @param {string} challengeTxSig Signature from the exiting output owner over the spend.
+   * @param {object} txOptions transaction options, such as `from`, gas` and `privateKey`
+   * @return {string} transaction hash of the call
+   */
+  async challengeStandardExit (outputId, challengeTx, inputIndex, challengeTxSig, txOptions) {
     const txDetails = {
-      from: fromAddress,
-      to: this.plasmaContractAddress,
-      data: this.plasmaContract.methods.startDepositExit(
-        depositPos,
-        token,
-        amount
-      ).encodeABI(),
-      gas: 2000000
-    }
-
-    return sendTx(this.eth, txDetails, privateKey)
-  }
-
-  async challengeExit (fromAddress, cUtxoPos, eUtxoIndex, txBytes, proof, sigs, privateKey) {
-    const txDetails = {
-      from: fromAddress,
+      from: txOptions.from,
       to: this.plasmaContractAddress,
       data: this.plasmaContract.methods.challengeExit(
-        cUtxoPos,
-        eUtxoIndex,
-        Web3Utils.hexToBytes(`0x${txBytes}`),
-        Web3Utils.hexToBytes(`0x${proof}`),
-        Web3Utils.hexToBytes(`0x${sigs}`)
+        outputId,
+        Web3Utils.hexToBytes(`0x${challengeTx}`),
+        inputIndex,
+        Web3Utils.hexToBytes(`0x${challengeTxSig}`)
       ).encodeABI(),
-      gas: 2000000
+      gas: txOptions.gas || 2000000
     }
 
-    return sendTx(this.eth, txDetails, privateKey)
+    return sendTx(this.eth, txDetails, txOptions.privateKey)
   }
 
-  async finalizeExits (fromAddress, token, topUtxoPos, exitsToProcess, privateKey) {
+  /**
+   * Processes any exits that have completed the challenge period.
+   * @method processExits
+   * @param {string} token Address of the token to process.
+   * @param {number} topUtxoPos First exit that should be processed. Set to zero to skip the check.
+   * @param {number} exitsToProcess Maximum number of exits to process.
+   * @param {object} txOptions transaction options, such as `from`, gas` and `privateKey`
+   * @return {string} transaction hash of the call
+   */
+  async processExits (token, topUtxoPos, exitsToProcess, txOptions) {
     const txDetails = {
-      from: fromAddress,
+      from: txOptions.from,
       to: this.plasmaContractAddress,
       data: this.plasmaContract.methods.finalizeExits(token, topUtxoPos, exitsToProcess).encodeABI(),
-      gas: 2000000
+      gas: txOptions.gas || 2000000
     }
 
-    return sendTx(this.eth, txDetails, privateKey)
+    return sendTx(this.eth, txDetails, txOptions.privateKey)
   }
 }
 
