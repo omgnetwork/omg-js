dd8956d203bf7ae4a0771c3a3e8d292818c685c1
Kevin Sullivan <kevsul@gmail.com>
2019-01-31 17:28:33 +0100

    Change createTransaction test to 4in/4out

--- packages/omg-js-childchain/test/createTransactionTest.js
+++ packages/omg-js-childchain/test/createTransactionTest.js
@@ -29,7 +29,7 @@ describe('createTransaction', () => {
     assert.equal(unsignedTx, expectedTx)
   })
 
-  it('should create a hex-encoded unsigned transaction with 2 inputs and 2 outputs', () => {
+  it('should create a hex-encoded unsigned transaction with 4 inputs and 4 outputs', () => {
     const txBody = {
       'inputs': [
         {
@@ -43,6 +43,18 @@ describe('createTransaction', () => {
           'oindex': 0,
           'currency': '0000000000000000000000000000000000000000',
           'blknum': 19774002
+        },
+        {
+          'txindex': 0,
+          'oindex': 1,
+          'currency': '0000000000000000000000000000000000000000',
+          'blknum': 19774002
+        },
+        {
+          'txindex': 10,
+          'oindex': 0,
+          'currency': '0000000000000000000000000000000000000000',
+          'blknum': 19774002
         }
       ],
       'outputs': [
@@ -53,12 +65,20 @@ describe('createTransaction', () => {
         {
           'owner': '0x3272ee86d8192f59261960c9ae186063c8c9041f',
           'amount': 1000000000000000000
+        },
+        {
+          'owner': '0x99d7b5c57c16acb24a327a37356a804a2f75dade',
+          'amount': 100000000
+        },
+        {
+          'owner': '0xbfdf85743ef16cfb1f8d4dd1dfc74c51dc496434',
+          'amount': 100
         }
       ]
     }
 
     const unsignedTx = childChain.createTransaction(txBody)
-    const expectedTx = 'f8b3d8c784012dba318080c784012dba328080c3808080c3808080f898df94f4ebbe787311bb955bb353b7a4d8b97af8ed1c9b80880de0b6b3a7640000df943272ee86d8192f59261960c9ae186063c8c9041f80880de0b6b3a7640000eb94000000000000000000000000000000000000000094000000000000000000000000000000000000000080eb94000000000000000000000000000000000000000094000000000000000000000000000000000000000080'
+    const expectedTx = 'f897e0c784012dba318080c784012dba328080c784012dba328001c784012dba320a80f874df94f4ebbe787311bb955bb353b7a4d8b97af8ed1c9b80880de0b6b3a7640000df943272ee86d8192f59261960c9ae186063c8c9041f80880de0b6b3a7640000db9499d7b5c57c16acb24a327a37356a804a2f75dade808405f5e100d794bfdf85743ef16cfb1f8d4dd1dfc74c51dc4964348064'
     assert.equal(unsignedTx, expectedTx)
   })
 
