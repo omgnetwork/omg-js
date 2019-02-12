5ad47ac78be4b54b3e58a6630c289e30475cda09
Kevin Sullivan <kevsul@gmail.com>
2019-02-12 10:56:29 +0100

    Remove unused omg-js-util files

--- packages/omg-js-util/src/index.js
+++ packages/omg-js-util/src/index.js
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 module.exports = {
-  byteArrToBuffer: require('./byteArrToBuffer'),
-  hexToByteArr: require('./hexToByteArr'),
   InvalidArgumentError: require('./InvalidArgumentError'),
   transaction: require('./transaction')
 }
