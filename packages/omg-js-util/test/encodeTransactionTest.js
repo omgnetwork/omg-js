53a2d465e8f5c36ffd8eea517cace5ea776dda85
jarindr <jarindr23@gmail.com>
2019-11-07 16:12:48 +0700

    fix encode transfer test

--- packages/omg-js-util/test/encodeTransactionTest.js
+++ packages/omg-js-util/test/encodeTransactionTest.js
@@ -13,4 +13,168 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-// TODO: add transactionALD tests
+const chai = require('chai')
+const assert = chai.assert
+const transaction = require('../src/transaction')
+
+describe.only('Transaction tests', () => {
+  it('should return an encoded transaction from 1 input and 1 output', async () => {
+    const txBody = {
+      inputs: [
+        {
+          txindex: 0,
+          oindex: 0,
+          blknum: 19774001
+        }
+      ],
+      outputs: [
+        {
+          outputType: 1,
+          outputGuard: '0xf4ebbe787311bb955bb353b7a4d8b97af8ed1c9b',
+          currency: '0x0000000000000000000000000000000000000000',
+          amount: 500000000000000123
+        }
+      ]
+    }
+    const unsignedTx = transaction.encode(txBody)
+    const expectedTx =
+      '0xf84180c8c784012dba318080f5f40194f4ebbe787311bb955bb353b7a4d8b97af8ed1c9b9400000000000000000000000000000000000000008806f05b59d3b2006480'
+    assert.equal(unsignedTx, expectedTx)
+  })
+
+  it('should create a hex-encoded unsigned transaction with 4 inputs and 4 outputs', () => {
+    const txBody = {
+      inputs: [
+        {
+          txindex: 0,
+          oindex: 0,
+          blknum: 19774001
+        },
+        {
+          txindex: 0,
+          oindex: 0,
+          blknum: 19774002
+        },
+        {
+          txindex: 0,
+          oindex: 1,
+          blknum: 19774002
+        },
+        {
+          txindex: 10,
+          oindex: 0,
+          blknum: 19774002
+        }
+      ],
+      outputs: [
+        {
+          outputType: 1,
+          outputGuard: '0xf4ebbe787311bb955bb353b7a4d8b97af8ed1c9b',
+          currency: '0x0000000000000000000000000000000000000000',
+          amount: 1000000000000000000
+        },
+        {
+          outputType: 1,
+          outputGuard: '0x3272ee86d8192f59261960c9ae186063c8c9041f',
+          currency: '0x0000000000000000000000000000000000000000',
+          amount: 1000000000000000000
+        },
+        {
+          outputType: 1,
+          outputGuard: '0x99d7b5c57c16acb24a327a37356a804a2f75dade',
+          currency: '0x0000000000000000000000000000000000000000',
+          amount: 100000000
+        },
+        {
+          outputType: 1,
+          outputGuard: '0xbfdf85743ef16cfb1f8d4dd1dfc74c51dc496434',
+          currency: '0x0000000000000000000000000000000000000000',
+          amount: 100
+        }
+      ]
+    }
+
+    const unsignedTx = transaction.encode(txBody)
+    const expectedTx =
+      '0xf8ed80e0c784012dba318080c784012dba328080c784012dba328001c784012dba320a80f8c8f40194f4ebbe787311bb955bb353b7a4d8b97af8ed1c9b940000000000000000000000000000000000000000880de0b6b3a7640000f401943272ee86d8192f59261960c9ae186063c8c9041f940000000000000000000000000000000000000000880de0b6b3a7640000f0019499d7b5c57c16acb24a327a37356a804a2f75dade9400000000000000000000000000000000000000008405f5e100ec0194bfdf85743ef16cfb1f8d4dd1dfc74c51dc4964349400000000000000000000000000000000000000006480'
+    assert.equal(unsignedTx, expectedTx)
+  })
+
+  it('should encode a transaction with no outputs', () => {
+    const txBody = {
+      inputs: [
+        {
+          txindex: 0,
+          oindex: 0,
+          blknum: 19774001
+        }
+      ],
+      outputs: []
+    }
+    const unsignedTx = transaction.encode(txBody)
+    const expectedTx = '0xcc80c8c784012dba318080c080'
+    assert.equal(unsignedTx, expectedTx)
+  })
+
+  describe('encodeDeposit', () => {
+    it('should return an encoded deposit transaction', async () => {
+      const address = '0x854951e37c68a99a52d9e3ae15e0cb62184a613e'
+      const amount = 333
+      const expected =
+        '0xF85301C0EFEE0194854951E37C68A99A52D9E3AE15E0CB62184A613E94000000000000000000000000000000000000000082014DA00000000000000000000000000000000000000000000000000000000000000000'
+
+      const encoded = transaction.encodeDeposit(
+        address,
+        amount,
+        transaction.ETH_CURRENCY
+      )
+      const hexed = `0x${encoded.toString('hex').toUpperCase()}`
+      assert.equal(hexed, expected)
+    })
+
+    it('should return an encoded deposit transaction when address does not start with 0x', async () => {
+      const address = '854951e37c68a99a52d9e3ae15e0cb62184a613e'
+      const amount = 333
+      const expected =
+        '0xF86901C0F844F84201A83835343935316533376336386139396135326439653361653135653063623632313834613631336594000000000000000000000000000000000000000082014DA00000000000000000000000000000000000000000000000000000000000000000'
+
+      const encoded = transaction.encodeDeposit(
+        address,
+        amount,
+        transaction.ETH_CURRENCY
+      )
+      const hexed = `0x${encoded.toString('hex').toUpperCase()}`
+      assert.equal(hexed, expected)
+    })
+
+    it('should return an encoded deposit transaction when amount as a string', async () => {
+      const address = '0x60c5bc2de80acda5016c1e81f61620adbc730dd2'
+      const amount = '1000000000000000000'
+      const expected =
+        '0xF85901C0F5F4019460C5BC2DE80ACDA5016C1E81F61620ADBC730DD2940000000000000000000000000000000000000000880DE0B6B3A7640000A00000000000000000000000000000000000000000000000000000000000000000'
+
+      const encoded = transaction.encodeDeposit(
+        address,
+        amount,
+        transaction.ETH_CURRENCY
+      )
+      const hexed = `0x${encoded.toString('hex').toUpperCase()}`
+      assert.equal(hexed, expected)
+    })
+
+    it('should return an encoded deposit transaction when currency does not start with 0x', async () => {
+      const address = '0x60c5bc2de80acda5016c1e81f61620adbc730dd2'
+      const amount = '3000000000000000000000'
+      const expected =
+        '0xF87001C0F84BF849019460C5BC2DE80ACDA5016C1E81F61620ADBC730DD2A83030303030303030303030303030303030303030303030303030303030303030303030303030303089A2A15D09519BE00000A00000000000000000000000000000000000000000000000000000000000000000'
+
+      const encoded = transaction.encodeDeposit(
+        address,
+        amount,
+        '0000000000000000000000000000000000000000'
+      )
+      const hexed = `0x${encoded.toString('hex').toUpperCase()}`
+      assert.equal(hexed, expected)
+    })
+  })
+})
