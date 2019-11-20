b7ffd73efda0fa8873f9a68304934962e043c22d
nicholasmueller <nicholasmueller91@gmail.com>
2019-11-20 17:01:41 +0700

    deposit erc20 example

--- examples/childchain-deposit-erc20.js
+++ examples/childchain-deposit-erc20.js
@@ -14,8 +14,8 @@
   limitations under the License.
 */
 
-const BigNumber = require('bignumber.js')
 const Web3 = require('web3')
+const erc20abi = require('human-standard-token-abi')
 
 const RootChain = require('../packages/omg-js-rootchain/src/rootchain')
 const ChildChain = require('../packages/omg-js-childchain/src/childchain')
@@ -29,45 +29,78 @@ const web3 = new Web3(new Web3.providers.HttpProvider(config.geth_url), null, { 
 
 const rootChain = new RootChain(web3, config.rootchain_plasma_contract_address)
 const childChain = new ChildChain({ watcherUrl: config.watcher_url, watcherProxyUrl: config.watcher_proxy_url })
+const erc20Contract = new web3.eth.Contract(erc20abi, config.erc20_contract)
 
 const aliceAddress = config.alice_eth_address
 const alicePrivateKey = config.alice_eth_address_private_key
 
-const depositAmount = BigNumber(web3.utils.toWei(config.alice_eth_deposit_amount, 'ether'))
+async function getERC20Balance (address) {
+  const txDetails = {
+    from: address,
+    to: config.erc20_contract,
+    data: erc20Contract.methods.balanceOf(address).encodeABI()
+  }
+  return web3.eth.call(txDetails)
+}
 
-async function depositErc20IntoPlasmaContract () {
-  let rootchainBalance = await web3.eth.getBalance(aliceAddress)
+async function approveERC20 (ownerAccount, ownerPrivateKey, spender, value) {
+  const txDetails = {
+    from: ownerAccount,
+    to: config.erc20_contract,
+    data: erc20Contract.methods.approve(spender, value).encodeABI(),
+    gas: 6000000
+  }
+  const signedTx = await web3.eth.accounts.signTransaction(txDetails, ownerPrivateKey)
+  return web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+}
+
+async function depositERC20IntoPlasmaContract () {
+  let rootchainERC20Balance = await getERC20Balance(aliceAddress)
   let childchainBalanceArray = await childChain.getBalance(aliceAddress)
+  let erc20Object = childchainBalanceArray.find(i => i.currency === config.erc20_contract)
+  let childchainERC20Balance = erc20Object
+    ? erc20Object.amount
+    : 0
 
-  console.log(`Alice's rootchain balance: ${web3.utils.fromWei(String(rootchainBalance), 'ether')} ETH`)
-  console.log(`Alice's childchain balance: ${childchainBalanceArray.length === 0 ? 0 : web3.utils.fromWei(String(childchainBalanceArray[0].amount))} ETH`)
+  console.log('ERC20 token used for this example: ', config.erc20_contract)
+  console.log(`Alice's rootchain ERC20 balance: ${web3.utils.hexToNumber(rootchainERC20Balance)}`)
+  console.log(`Alice's childchain ERC20 balance: ${childchainERC20Balance}`)
   console.log('-----')
 
-  const depositTransaction = transaction.encodeDeposit(aliceAddress, depositAmount, transaction.ETH_CURRENCY)
+  const erc20VaultAddress = await rootChain.getErc20VaultAddress()
+  await approveERC20(aliceAddress, alicePrivateKey, erc20VaultAddress, config.alice_erc20_deposit_amount)
+  console.log('ERC20 approved')
 
-  console.log(`Depositing ${web3.utils.fromWei(depositAmount.toString(), 'ether')} ETH from the rootchain to the childchain`)
+  const depositTransaction = transaction.encodeDeposit(aliceAddress, config.alice_erc20_deposit_amount, config.erc20_contract)
 
-  // deposit ETH into the Plasma contract
-  const transactionReceipt = await rootChain.depositEth(depositTransaction, depositAmount, {
+  console.log(`Depositing ${config.alice_erc20_deposit_amount} ERC20 from the rootchain to the childchain`)
+
+  // deposit ERC20 into the Plasma contract
+  await rootChain.depositToken(depositTransaction, {
     from: aliceAddress,
-    privateKey: alicePrivateKey
+    privateKey: alicePrivateKey,
+    gas: 6000000
   })
   console.log('Deposit successful')
   console.log('Waiting for transaction to be recorded by the watcher...')
+  await wait.wait(40000)
 
-  await wait.waitForTransaction(web3, transactionReceipt.transactionHash, config.millis_to_wait_for_next_block, config.blocks_to_wait_for_txn)
-
-  rootchainBalance = await web3.eth.getBalance(aliceAddress)
+  rootchainERC20Balance = await getERC20Balance(aliceAddress)
   childchainBalanceArray = await childChain.getBalance(aliceAddress)
+  erc20Object = childchainBalanceArray.find(i => i.currency === config.erc20_contract)
+  childchainERC20Balance = erc20Object
+    ? erc20Object.amount
+    : 0
+
   console.log('-----')
-  console.log(`Alice's rootchain balance: ${web3.utils.fromWei(String(rootchainBalance), 'ether')} ETH`)
-  console.log(`Alice's childchain balance: ${childchainBalanceArray.length === 0 ? 0 : web3.utils.fromWei(String(childchainBalanceArray[0].amount))} ETH`)
+  console.log(`Alice's rootchain ERC20 balance: ${web3.utils.hexToNumber(rootchainERC20Balance)}`)
+  console.log(`Alice's childchain ERC20 balance: ${childchainERC20Balance}`)
   return Promise.resolve()
 }
 
 (async () => {
   try {
-    const result = await depositErc20IntoPlasmaContract()
+    const result = await depositERC20IntoPlasmaContract()
     return Promise.resolve(result)
   } catch (error) {
     console.log(error)
