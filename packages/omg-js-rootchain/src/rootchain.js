4f9f3206a55e0b8d95241158a767dd19d1f268a4
nicholasmueller <nicholasmueller91@gmail.com>
2019-12-02 14:00:40 +0700

    fix approveErc20 using wrong contract

--- packages/omg-js-rootchain/src/rootchain.js
+++ packages/omg-js-rootchain/src/rootchain.js
@@ -16,6 +16,7 @@ limitations under the License. */
 const txUtils = require('./txUtils')
 const { transaction } = require('@omisego/omg-js-util')
 const webUtils = require('web3-utils')
+const erc20abi = require('human-standard-token-abi')
 
 const ETH_VAULT_ID = 1
 const ERC20_VAULT_ID = 2
@@ -108,11 +109,12 @@ class RootChain {
     amount,
     txOptions
   }) {
-    const { contract, address } = await this.getErc20Vault()
+    const { address: spender } = await this.getErc20Vault()
+    const erc20Contract = this.getContract(erc20abi, erc20Address)
     const txDetails = {
       from: txOptions.from,
       to: erc20Address,
-      data: contract.methods.approve(address, amount).encodeABI(),
+      data: erc20Contract.methods.approve(spender, amount).encodeABI(),
       gas: txOptions.gas,
       gasPrice: txOptions.gasPrice
     }
@@ -183,6 +185,20 @@ class RootChain {
       txDetails,
       privateKey: txOptions.privateKey
     })
+  }
+
+  /**
+   * Get standard exit id to use when processing exits
+   *
+   * @method getStandardExitId
+   * @param {string} txBytes txBytes from the standard exit
+   * @param {string} utxoPos the UTXO position of the UTXO being exited
+   * @param {boolean} isDeposit whether the standard exit is of a deposit UTXO
+   */
+  async getStandardExitId ({ txBytes, utxoPos, isDeposit }) {
+    const { contract } = await this.getPaymentExitGame()
+    const exitId = await contract.methods.getStandardExitId(isDeposit, txBytes, utxoPos).call()
+    return exitId
   }
 
   /**
