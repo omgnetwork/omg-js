edf0acfd8b14265ed9c9595efd4d817856f0d883
nicholasmueller <nicholasmueller91@gmail.com>
2019-12-18 12:42:48 +0700

    fix another example

--- examples/childchain-transaction-eth.js
+++ examples/childchain-transaction-eth.js
@@ -14,7 +14,7 @@
   limitations under the License.
 */
 
-const BigNumber = require('bignumber.js')
+const BigNumber = require('bn.js')
 const Web3 = require('web3')
 const ChildChain = require('../packages/omg-js-childchain/src/childchain')
 const { transaction, waitForChildchainBalance } = require('../packages/omg-js-util/src')
@@ -51,8 +51,8 @@ async function createSignBuildAndSubmitTransaction () {
   const { bobETHBalance } = await logBalances()
   console.log('-----')
 
-  const transferAmount = BigNumber(web3.utils.toWei(config.alice_eth_transfer_amount, 'ether'))
-  const feeAmount = BigNumber(web3.utils.toWei('0.00000000000000001', 'ether'))
+  const transferAmount = new BigNumber(web3.utils.toWei(config.alice_eth_transfer_amount, 'ether'))
+  const feeAmount = new BigNumber(web3.utils.toWei('0.00000000000000001', 'ether'))
 
   const payments = [{
     owner: bobAddress,
