7c6671e08b7ee22b159c5c9a2542e59ea1e9afcc
nicholasmueller <nicholasmueller91@gmail.com>
2020-01-24 10:53:31 +0700

    improve mocha config, run tests in sequence for better debugging

---
+++ packages/integration-tests/tests.js
@@ -0,0 +1,19 @@
+// add new test files to the list below
+// mocha will run these in order
+
+require('./test/CORSHeaderTest')
+require('./test/metadataTest')
+require('./test/decodeTxBytesTest')
+require('./test/addTokenTest')
+require('./test/depositTest')
+require('./test/createTransactionTest')
+require('./test/transferTest')
+require('./test/createSubmitTypedTransactionTest')
+require('./test/getExitQueueTest')
+require('./test/standardExitTest')
+require('./test/challengeExitTest')
+require('./test/inFlightExitTest')
+require('./test/inFlightExitChallengeTest')
+require('./test/inFlightExitChallengeResponse')
+require('./test/challengeInFlightExitOutputSpentTest')
+require('./test/challengeInFlightExitInputSpentTest')
