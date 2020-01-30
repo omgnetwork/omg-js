c9b74724b9d79281e9986d328f7b9e18b45fd1f3
nicholasmueller <nicholasmueller91@gmail.com>
2020-01-30 17:44:52 +0700

    update to new test format

--- packages/integration-tests/test/amountTypes.js
+++ packages/integration-tests/test/amountTypes.js
@@ -17,46 +17,51 @@ const ChildChain = require('@omisego/omg-js-childchain')
 const Web3 = require('web3')
 const erc20abi = require('human-standard-token-abi')
 
-const faucet = require('../helpers/testFaucet')
+const faucet = require('../helpers/faucet')
 const config = require('../test-config')
 const rcHelper = require('../helpers/rootChainHelper')
 
 should()
 use(chaiAsPromised)
 
+const path = require('path')
+const faucetName = path.basename(__filename)
+
 describe('amountTypes.js (ci-enabled)', function () {
-  const web3 = new Web3(new Web3.providers.HttpProvider(config.geth_url))
+  const web3 = new Web3(new Web3.providers.HttpProvider(config.eth_node))
+  const rootChain = new RootChain({ web3, plasmaContractAddress: config.plasmaframework_contract_address })
   const childChain = new ChildChain({ watcherUrl: config.watcher_url, watcherProxyUrl: config.watcher_proxy_url })
-  let rootChain
+  const testErc20Contract = new web3.eth.Contract(erc20abi, config.erc20_contract_address)
+
   let aliceAccount
   const INTIIAL_ALICE_AMOUNT = web3.utils.toWei('0.5', 'ether')
   const INITIAL_AMOUNT_ERC20 = 3
-  const testErc20Contract = new web3.eth.Contract(erc20abi, config.testErc20Contract)
 
   before(async function () {
-    const plasmaContract = await rcHelper.getPlasmaContractAddress(config)
-    rootChain = new RootChain({ web3, plasmaContractAddress: plasmaContract.contract_addr })
-    await faucet.init(rootChain, childChain, web3, config)
+    await faucet.init({ rootChain, childChain, web3, config, faucetName })
   })
 
   beforeEach(async function () {
     aliceAccount = rcHelper.createAccount(web3)
-    console.log(`Created new account ${JSON.stringify(aliceAccount)}`)
-    const plasmaContract = await rcHelper.getPlasmaContractAddress(config)
-    rootChain = new RootChain({ web3, plasmaContractAddress: plasmaContract.contract_addr })
-
-    await faucet.init(rootChain, childChain, web3, config)
-    await faucet.fundRootchainEth(web3, aliceAccount.address, INTIIAL_ALICE_AMOUNT)
-    await faucet.fundRootchainERC20(web3, aliceAccount.address, INITIAL_AMOUNT_ERC20, testErc20Contract)
+    await faucet.fundRootchainEth(aliceAccount.address, INTIIAL_ALICE_AMOUNT)
+    await faucet.fundRootchainERC20(aliceAccount.address, INITIAL_AMOUNT_ERC20, testErc20Contract)
     await Promise.all([
       rcHelper.waitForEthBalanceEq(web3, aliceAccount.address, INTIIAL_ALICE_AMOUNT),
-      rcHelper.waitForERC20BalanceEq(web3, aliceAccount.address, config.testErc20Contract, INITIAL_AMOUNT_ERC20)
+      rcHelper.waitForERC20BalanceEq(web3, aliceAccount.address, config.erc20_contract_address, INITIAL_AMOUNT_ERC20)
     ])
+  })
+
+  afterEach(async function () {
+    try {
+      await faucet.returnFunds(aliceAccount)
+    } catch (err) {
+      console.warn(`Error trying to return funds to the faucet: ${err}`)
+    }
   })
 
   it('approveToken() should only accept safe integers and strings and BN', async function () {
     const numberReceipt = await rootChain.approveToken({
-      erc20Address: config.testErc20Contract,
+      erc20Address: config.erc20_contract_address,
       amount: 10,
       txOptions: {
         from: aliceAccount.address,
@@ -66,7 +71,7 @@ describe('amountTypes.js (ci-enabled)', function () {
     assert.hasAnyKeys(numberReceipt, ['transactionHash'])
 
     const stringReceipt = await rootChain.approveToken({
-      erc20Address: config.testErc20Contract,
+      erc20Address: config.erc20_contract_address,
       amount: '999999999999999999999999',
       txOptions: {
         from: aliceAccount.address,
@@ -76,7 +81,7 @@ describe('amountTypes.js (ci-enabled)', function () {
     assert.hasAnyKeys(stringReceipt, ['transactionHash'])
 
     const bnReceipt = await rootChain.approveToken({
-      erc20Address: config.testErc20Contract,
+      erc20Address: config.erc20_contract_address,
       amount: web3.utils.toBN(1),
       txOptions: {
         from: aliceAccount.address,
@@ -86,7 +91,7 @@ describe('amountTypes.js (ci-enabled)', function () {
     assert.hasAnyKeys(bnReceipt, ['transactionHash'])
 
     const unsafeReceipt = rootChain.approveToken({
-      erc20Address: config.testErc20Contract,
+      erc20Address: config.erc20_contract_address,
       amount: 999999999999999999999999999999999999999999999999999999999999,
       txOptions: {
         from: aliceAccount.address,
@@ -96,7 +101,7 @@ describe('amountTypes.js (ci-enabled)', function () {
     assert.isRejected(unsafeReceipt)
 
     const decimalReceipt = rootChain.approveToken({
-      erc20Address: config.testErc20Contract,
+      erc20Address: config.erc20_contract_address,
       amount: 1.35,
       txOptions: {
         from: aliceAccount.address,
