f615a7da5cb2710e75674af5bd1cac39999ef056
euro <phuchit@omisego.co>
2020-03-17 01:54:18 +0700

    Add shim

--- packages/react-native-omg-js/index.js
+++ packages/react-native-omg-js/index.js
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+require('./shim')
 const RootChain = require('@omisego/omg-js-rootchain')
 const ChildChain = require('@omisego/omg-js-childchain')
 const OmgUtil = require('@omisego/omg-js-util')
