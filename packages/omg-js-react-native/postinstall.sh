90657746f648d9b043c6f0ad793fdde0c7d40d13
nicholasmueller <nicholasmueller91@gmail.com>
2020-03-16 16:20:01 +0700

    fix: master euro -> remove pbcopy

--- packages/omg-js-react-native/postinstall.sh
+++ packages/omg-js-react-native/postinstall.sh
@@ -4,6 +4,22 @@
 rn-nodeify --install assert,stream,events,crypto,url,http,https,vm,os,fs,path,process,net,zlib,_stream_transform,tls --hack
 
 # Polyfill TextEncoder from joi
-cat node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js | pbcopy && echo "require('fast-text-encoding');" > node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js && pbpaste >> node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js
-cat node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js | pbcopy && echo "require('fast-text-encoding');" > node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js && pbpaste >> node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js
-cat node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js | pbcopy && echo "require('fast-text-encoding');" > node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js && pbpaste >> node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js
+# Backup original content
+cat node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js > node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/tmp.js
+cat node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js > node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/tmp.js
+cat node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js > node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/tmp.js
+
+# Replace joi-browser.min.js content
+echo "require('fast-text-encoding');" > node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js 
+echo "require('fast-text-encoding');" > node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js 
+echo "require('fast-text-encoding');" > node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js 
+
+# Append original content
+cat node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/tmp.js >> node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js
+cat node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/tmp.js >> node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js
+cat node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/tmp.js >> node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js
+
+# Remove tmp file
+rm node_modules/@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/tmp.js
+rm node_modules/@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/tmp.js
+rm node_modules/@omisego/omg-js-util/node_modules/@hapi/joi/dist/tmp.js
