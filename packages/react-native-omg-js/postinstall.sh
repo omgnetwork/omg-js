663d19a3b70d2836b2792f994afedd2a3925ebd7
euro <phuchit@omisego.co>
2020-03-17 11:21:41 +0700

    Fix wrong path concat

--- packages/react-native-omg-js/postinstall.sh
+++ packages/react-native-omg-js/postinstall.sh
@@ -9,21 +9,21 @@ fi
 
 # Polyfill TextEncoder from joi
 # Backup original content
-cat $BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/tmp.js
-cat $BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/tmp.js
-cat $BASE_PATH/omg-js-util/node_modules/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-util/node_modules/$JOI_PATH/tmp.js
+cat $BASE_PATH/omg-js-rootchain/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-rootchain/$JOI_PATH/tmp.js
+cat $BASE_PATH/omg-js-childchain/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-childchain/$JOI_PATH/tmp.js
+cat $BASE_PATH/omg-js-util/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-util/$JOI_PATH/tmp.js
 
 # Replace joi-browser.min.js content
-echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/joi-browser.min.js
-echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/joi-browser.min.js
-echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-util/node_modules/$JOI_PATH/joi-browser.min.js
+echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-rootchain/$JOI_PATH/joi-browser.min.js
+echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-childchain/$JOI_PATH/joi-browser.min.js
+echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-util/$JOI_PATH/joi-browser.min.js
 
 # Append original content
-cat $BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/joi-browser.min.js
-cat $BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/joi-browser.min.js
-cat $BASE_PATH/omg-js-util/node_modules/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-util/node_modules/$JOI_PATH/joi-browser.min.js
+cat $BASE_PATH/omg-js-rootchain/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-rootchain/$JOI_PATH/joi-browser.min.js
+cat $BASE_PATH/omg-js-childchain/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-childchain/$JOI_PATH/joi-browser.min.js
+cat $BASE_PATH/omg-js-util/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-util/$JOI_PATH/joi-browser.min.js
 
 # Remove tmp file
-rm $BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/tmp.js
-rm $BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/tmp.js
-rm $BASE_PATH/omg-js-util/node_modules/$JOI_PATH/tmp.js
+rm $BASE_PATH/omg-js-rootchain/$JOI_PATH/tmp.js
+rm $BASE_PATH/omg-js-childchain/$JOI_PATH/tmp.js
+rm $BASE_PATH/omg-js-util/$JOI_PATH/tmp.js
