f397a3ef3e82998c20291f4761d354b37136beff
euro <phuchit@omisego.co>
2020-03-17 11:19:23 +0700

    Improve postinstall script

--- packages/react-native-omg-js/postinstall.sh
+++ packages/react-native-omg-js/postinstall.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
-if [ "$CI" = true ]; then
+
+BASE_PATH=../../@omisego
+JOI_PATH=node_modules/@hapi/joi/dist
+
+if [ ! -d "$BASE_PATH" ]; then
   exit 0
 fi
 
 # Polyfill TextEncoder from joi
 # Backup original content
-cat ../../@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js >../../@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/tmp.js
-cat ../../@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js >../../@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/tmp.js
-cat ../../@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js >../../@omisego/omg-js-util/node_modules/@hapi/joi/dist/tmp.js
+cat $BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/tmp.js
+cat $BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/tmp.js
+cat $BASE_PATH/omg-js-util/node_modules/$JOI_PATH/joi-browser.min.js >$BASE_PATH/omg-js-util/node_modules/$JOI_PATH/tmp.js
 
 # Replace joi-browser.min.js content
-echo "require('fast-text-encoding');" >../../@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js
-echo "require('fast-text-encoding');" >../../@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js
-echo "require('fast-text-encoding');" >../../@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js
+echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/joi-browser.min.js
+echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/joi-browser.min.js
+echo "require('fast-text-encoding');" >$BASE_PATH/omg-js-util/node_modules/$JOI_PATH/joi-browser.min.js
 
 # Append original content
-cat ../../@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/tmp.js >>../../@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/joi-browser.min.js
-cat ../../@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/tmp.js >>../../@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/joi-browser.min.js
-cat ../../@omisego/omg-js-util/node_modules/@hapi/joi/dist/tmp.js >>../../@omisego/omg-js-util/node_modules/@hapi/joi/dist/joi-browser.min.js
+cat $BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/joi-browser.min.js
+cat $BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/joi-browser.min.js
+cat $BASE_PATH/omg-js-util/node_modules/$JOI_PATH/tmp.js >>$BASE_PATH/omg-js-util/node_modules/$JOI_PATH/joi-browser.min.js
 
 # Remove tmp file
-rm ../../@omisego/omg-js-rootchain/node_modules/@hapi/joi/dist/tmp.js
-rm ../../@omisego/omg-js-childchain/node_modules/@hapi/joi/dist/tmp.js
-rm ../../@omisego/omg-js-util/node_modules/@hapi/joi/dist/tmp.js
+rm $BASE_PATH/omg-js-rootchain/node_modules/$JOI_PATH/tmp.js
+rm $BASE_PATH/omg-js-childchain/node_modules/$JOI_PATH/tmp.js
+rm $BASE_PATH/omg-js-util/node_modules/$JOI_PATH/tmp.js
