95a7c718c9a1746b23ecdb9c9bde3e24db006a37
jarindr <jarindr23@gmail.com>
2020-03-18 14:33:21 +0700

    improve-example

--- examples/childchain-merge-utxos.js
+++ examples/childchain-merge-utxos.js
@@ -18,7 +18,7 @@ const ChildChain = require('../packages/omg-js-childchain/src/childchain')
 const transaction = require('../packages/omg-js-util/src/transaction')
 const config = require('./config.js')
 const JSONBigNumber = require('omg-json-bigint')
-
+const wait = require('./wait.js')
 const childChain = new ChildChain({
   watcherUrl: config.watcher_url,
   watcherProxyUrl: config.watcher_proxy_url
@@ -34,13 +34,15 @@ async function childchainMergeUtxos () {
   const utxosToMerge = aliceUtxos
     .filter((u) => u.currency === transaction.ETH_CURRENCY)
     .slice(0, 4)
-  const result = await childChain.mergeUtxos({
+  const utxo = await childChain.mergeUtxos({
     utxos: utxosToMerge,
     privateKey: config.alice_eth_address_private_key,
     verifyingContract: config.plasmaframework_contract_address
   })
 
-  console.log('merge utxo transaction result', result)
+  console.log('merge utxo transaction result', utxo)
+  await wait.waitForUtxo(utxo)
+  console.log(`Alice UTXOs has length of ${aliceUtxos.length}`)
 }
 
 childchainMergeUtxos()
