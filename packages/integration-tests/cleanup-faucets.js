b1c0fccd9244086bb66d6f5b7373df6e1923a45c
Khun Pong <k.cheecharern@gmail.com>
2020-03-28 12:26:24 +0700

    fix faucet cleanup

--- packages/integration-tests/cleanup-faucets.js
+++ packages/integration-tests/cleanup-faucets.js
@@ -25,7 +25,7 @@ const config = require('./test-config')
 
 const web3 = new Web3(new Web3.providers.HttpProvider(config.eth_node))
 const rootChain = new RootChain({ web3, plasmaContractAddress: config.plasmaframework_contract_address })
-const childChain = new ChildChain({ watcherUrl: config.watcher_url, watcherProxyUrl: config.watcher_proxy_url })
+const childChain = new ChildChain({ watcherUrl: config.watcher_url, watcherProxyUrl: config.watcher_proxy_url, plasmaContractAddress: config.plasmaframework_contract_address })
 
 async function cleanup () {
   console.log(`🧹 Returning faucet funds back to the fund account: ${config.fund_account}`)
