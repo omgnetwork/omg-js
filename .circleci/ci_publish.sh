8c446c89e4ab59cb0bed64b9f98d545e2a0eee6e
boolafish <boolafish945@gmail.com>
2020-08-18 18:41:54 +0900

    debug: auth in config, other in script

--- .circleci/ci_publish.sh
+++ .circleci/ci_publish.sh
@@ -1,11 +1,9 @@
+#!/bin/sh
+
 set -e
-set -x
 
 GIT_COMMIT_SHA=$(git rev-parse --short=7 HEAD)
 TEST_RUNNER_TAG=gcr.io/omisego-development/omg-js-testrunner:$GIT_COMMIT_SHA
-
-echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
-gcloud -q auth configure-docker
 
 docker build -t testrunner .
 docker tag testrunner $TEST_RUNNER_TAG
