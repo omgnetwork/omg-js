f7eb36369902ce6803985b2e3c9b1d7ef5b9022c
nicholasmueller <nicholasmueller91@gmail.com>
2020-10-09 11:00:38 +0700

    type status

--- src/watcher/status.ts
+++ src/watcher/status.ts
@@ -1,8 +1,26 @@
-import * as Interfaces from '@lib/common/interfaces';
 import * as Transporter from '@lib/transport';
 
-// NMTODO: figure out this interface
-export interface IStatus {};
+export interface IStatus {
+  byzantine_events: Array<any>;
+  contract_addr: {
+    erc20_vault: string;
+    eth_vault: string;
+    payment_exit_game: string;
+    plasma_framework: string;
+  };
+  eth_syncing: boolean;
+  in_flight_exits: Array<any>;
+  last_mined_child_block_number: number;
+  last_mined_child_block_timestamp: number;
+  last_seen_eth_block_number: number;
+  last_seen_eth_block_timestamp: number;
+  last_validated_child_block_number: number;
+  last_validated_child_block_timestamp: number;
+  services_synced_heights: Array<{
+    height: number;
+    service: string;
+  }>
+};
 
 export async function getStatus (): Promise<IStatus> {
   return Transporter.post({
